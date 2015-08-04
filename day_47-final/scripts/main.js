d18610715f16a015691d2bd28abec0acda5e25a5
Andrea St. Clair <andreadoris86@gmail.com>
2015-08-03 23:47:14 -0500

    updated styles, added jquery to favorites

---
+++ day_47-final/scripts/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.favorite').click(function(){
+		if(!$(this).hasClass('iLoveIt')){
+			$(this).addClass('iLoveIt');
+			$(this).attr('src', '/images/favorite.svg');
+		}
+		else {
+			$(this).removeClass('iLoveIt');
+			$(this).attr('src', '/images/favorite.png');
+		}
+	})
+});
