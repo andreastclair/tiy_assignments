cd4d35e153d0ef2899d317197f4d6e76df423c56
Andrea St. Clair <andreadoris86@gmail.com>
2015-08-05 19:35:15 -0500

    fixed path error

--- day_47-final/scripts/main.js
+++ day_47-final/scripts/main.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	$('.favorite').click(function(){
 		if(!$(this).hasClass('iLoveIt')){
 			$(this).addClass('iLoveIt');
-			$(this).attr('src', '/images/favorite-yellow.png');
+			$(this).attr('src', 'images/favorite-yellow.png');
 			// $(this).attr('src', '/images/favorite.svg');
 
 		}
 		else {
 			$(this).removeClass('iLoveIt');
-			$(this).attr('src', '/images/favorite.png');
+			$(this).attr('src', 'images/favorite.png');
 		}
 	})
 });
